c1670eec8d8b98ff287a734b0412026d66f14e9d
unknown <tjdnjswhd1@naver.com>
2021-01-10 14:08:42 +0900

    BOJ. S1. 12852. '1로 만들기 2'. Timeout.

--- CodingTest/12852_'1로 만들기 2'.cpp
+++ CodingTest/12852_'1로 만들기 2'.cpp
@@ -16,7 +16,7 @@ int main(int argc, char* argv[]) {
 	cin >> N;
 
 	dp[1] = { 0, 0 };
-	dp[2] = dp[3] = { 1, 1 };
+	dp[2] = dp[3] = { 1, 1 };;
 
 	/*for (int i = 4; i <= N; i++) {
 		if (i % 2 == 0 && i % 3 == 0) {
