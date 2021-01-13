23ceba5e365f78145dad5c57c5fc3f55cbb77622
unknown <tjdnjswhd1@naver.com>
2021-01-13 17:18:29 +0900

    BOJ. S2. 11279. '최대 힙'. Solved.

---
+++ CodingTest/11279_'최대 힙'.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <queue>
+
+#define endl '\n'
+
+using namespace std;
+
+int N;
+
+int main(int argc, char* argv[]) {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	priority_queue<int, vector<int>, less<int>> PQ;
+
+	cin >> N;
+
+	while (N--) {
+		int x; cin >> x;
+
+		if (x > 0) {
+			PQ.push(x);
+		}
+		else {
+			if (PQ.empty())
+				cout << 0 << endl;
+			else {
+				cout << PQ.top() << endl;
+				PQ.pop();
+			}
+		}
+	}
+
+	return 0;
+}
