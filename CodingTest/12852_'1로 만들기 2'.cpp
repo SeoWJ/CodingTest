c148bd423f226cd4d77d14358490fb0d25fe7d5f
unknown <tjdnjswhd1@naver.com>
2021-02-24 13:12:36 +0900

    BOJ. S1. 12852. '1로 만들기 2'. Failed.

--- CodingTest/12852_'1로 만들기 2'.cpp
+++ CodingTest/12852_'1로 만들기 2'.cpp
@@ -4,73 +4,45 @@
 
 using namespace std;
 
-typedef pair<int, int> pii;
-
-int N;
-pii dp[1000001] = { {0,0} };
+int DP[1000001] = { 0 };
+int before[1000001] = { 0 };
 
 int main(int argc, char* argv[]) {
 	ios::sync_with_stdio(false);
-	cin.tie(0);
+	cin.tie(NULL);
 
-	cin >> N;
+	int N; cin >> N;
 
-	dp[1] = { 0, 0 };
-	dp[2] = dp[3] = { 1, 1 };;
-
-	/*for (int i = 4; i <= N; i++) {
-		if (i % 2 == 0 && i % 3 == 0) {
-			dp[i] = min3(i - 1, i / 2, i / 3);
-		}
-		else if (i % 2 == 0 && i % 3 != 0)
-			dp[i] = min2(i - 1, i / 2);
-		else if (i % 2 != 0 && i % 3 == 0)
-			dp[i] = min2(i - 1, i / 3);
-		else
-			dp[i] = { dp[i - 1].first + 1, i - 1 };
-	}*/
+	DP[1] = 0;
+	DP[2] = DP[3] = before[2] = before[3] = 1;
 
 	for (int i = 4; i <= N; i++) {
-		if (i % 3 == 0) {
-			if (i % 2 == 0) {
-				int min = dp[i / 3].first < dp[i / 2].first ? dp[i / 3].first : dp[i / 2].first;
-				int prev = dp[i / 3].first < dp[i / 2].first ? i / 3 : i / 2;
-				prev = min < dp[i - 1].first ? prev : i - 1;
-				min = min < dp[i - 1].first ? min : dp[i - 1].first;
+		int prev;
 
-				dp[i] = { min + 1, prev };
-			}
-			else {
-				int prev = dp[i / 3].first < dp[i - 1].first ? i / 3 : i - 1;
-				int min = dp[i / 3].first < dp[i - 1].first ? dp[i / 3].first : dp[i - 1].first;
+		if (i % 3 == 0 && i % 2 == 0)
+			prev = DP[i / 3] < DP[i / 2] ? (DP[i / 3] < DP[i - 1] ? i / 3 : i - 1) : (DP[i / 2] < DP[i - 1] ? i / 2 : i - 1);
+		else if (i % 3 != 0 && i % 2 == 0)
+			prev = DP[i / 2] < DP[i - 1] ? i / 2 : i - 1;
+		else if (i % 3 == 0 && i % 2 != 0)
+			prev = DP[i / 3] < DP[i - 1] ? i / 3 : i - 1;
+		else
+			prev = i - 1;
 
-				dp[i] = { min + 1, prev };
-			}
-		}
-		else if (i % 2 == 0) {
-			int prev = dp[i / 2].first < dp[i - 1].first ? i / 2 : i - 1;
-			int min = dp[i / 2].first < dp[i - 1].first ? dp[i / 2].first : dp[i - 1].first;
-
-			dp[i] = { min + 1, prev };
-		}
-		else {
-			dp[i] = { dp[i - 1].first + 1, i - 1 };
-		}
+		DP[i] = DP[prev] + 1;
+		before[i] = prev;
 	}
 
-	cout << dp[N].first << endl;
+	cout << DP[N] << endl;
 
 	cout << N << " ";
-	int i = dp[N].second;
+	int i = N;
 	while (1) {
-		if (i == 1) {
-			cout << i << endl;
-			break;
-		}
+		cout << before[i] << " ";
+		i = before[i];
 
-		cout << i << " ";
-		i = dp[i].second;
+		if (i == 1) break;
 	}
+	cout << endl;
 
 	return 0;
 }
